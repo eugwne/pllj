a7f49f94804524e4fa1151b48b30c4947cf54e33
Eugene Sergeev <eugeney.sergeev@gmail.com>
2018-10-03 21:25:19 +0300

    spi prepare plan (#12)

--- src/pllj/spi.lua
+++ src/pllj/spi.lua
@@ -8,6 +8,9 @@ local NULL = ffi.NULL
 
 local pgdef = require('pllj.pgdefines')
 
+local call_pg_variadic = require('pllj.pg.func').call_pg_variadic
+local text_to_pg = require('pllj.pg.to_pg').datumfor[C.TEXTOID]
+
 local function throw_error(...)
     spi.disconnect()
     error(...)
@@ -25,44 +28,103 @@ end
 local pg_error = require('pllj.pg.pg_error')
 
 local to_lua = require('pllj.io').to_lua
+local to_pg = require('pllj.io').to_pg
 
 local tuple_to_lua_1array = require('pllj.tuple_ops').tuple_to_lua_1array
 
-function spi.execute(query)
-  local result = -1
-  --try
-  result = C.lj_SPI_execute(query, 0, 0)
-  --catch
-  if (result < 0) then
-    if (result == pg_error.THROW_NUMBER) then
-      return error("SPI_execute_plan error:"..pg_error.get_exception_text())
-    end
-    return error("SPI_execute_plan error:"..tostring(query))
-  end
-  if ((result == pgdef.spi["SPI_OK_SELECT"]) and (C.SPI_processed > 0)) then
-    local tupleDesc = C.SPI_tuptable.tupdesc --[[TupleDesc]]
+
+local function process_query_result(result)
+    if (result < 0) then
+        if (result == pg_error.THROW_NUMBER) then
+          return error("SPI execute error: "..pg_error.get_exception_text())
+        end
+        return error("SPI execute error: "..tostring(query))
+      end
+      if ((result == pgdef.spi["SPI_OK_SELECT"]) and (C.SPI_processed > 0)) then
+        local tupleDesc = C.SPI_tuptable.tupdesc --[[TupleDesc]]
+        
     
+        local rows = {}
+        local spi_processed = tonumber(C.SPI_processed)
+        for i = 0, spi_processed-1 do
+          local tuple = C.SPI_tuptable.vals[i] --[[HeapTuplelocal]]
+          rows[i+1] = tuple_to_lua_1array(tupleDesc, tuple)
+    
+        end
+    
+        C.SPI_freetuptable(C.SPI_tuptable);
+        return rows
+    
+      else
+        return {}
+      end
+end
 
-    local rows = {}
-    local spi_processed = tonumber(C.SPI_processed)
-    for i = 0, spi_processed-1 do
-      local tuple = C.SPI_tuptable.vals[i] --[[HeapTuplelocal]]
-      rows[i+1] = tuple_to_lua_1array(tupleDesc, tuple)
-
-    end
-
-    C.SPI_freetuptable(C.SPI_tuptable);
-    return rows
-
-  else
-    return {}
-  end
+function spi.execute(query)
+    local result = -1
+    --try
+    result = C.lj_SPI_execute(query, 0, 0)
+    --catch
+    return process_query_result(result)
 
 
 end
 
+local function exec_plan(prepared_plan, ...)
+    local argc = prepared_plan.argc
+    local oids = prepared_plan.oids
+    local values = ffi.new("Datum [?]", argc)
+    local nulls = ffi.new("char [?]", argc)
+    local has_nulls = false
+    for i = 0, argc-1 do
+        local v = select(i+1, ...)
+        if v and v ~= ffi.NULL then
+            nulls[i] = string.byte(' ')
+            values[i] = to_pg(oids[i])(v)
+        else
+            nulls[i] = string.byte('n')
+            has_nulls = true
+        end
+    end
+    if not has_nulls then
+        nulls = nil
+    end
+    local result = C.lj_SPI_execute_plan(prepared_plan.plan, values, nulls, 0, 0)
+    return process_query_result(result)
+end
+
+local plan_mt = {
+    __index = {
+        exec = exec_plan,
+    },
+  }
+
+function spi.prepare(query, arg_types)
+    local argc = #arg_types
+    local oids = ffi.new("Oid [?]", argc)
+    for i = 1, argc do
+        local oid = call_pg_variadic(C.to_regtype, {text_to_pg(arg_types[i])})
+        oids[i-1] = oid
+    end
+    local plan = C.lj_SPI_prepare_cursor(query, #arg_types, oids, 0)
+    if plan == nil then
+        return error("SPI_prepare_cursor error:"..pg_error.get_exception_text())
+    end
+    assert(C.SPI_keepplan(plan)==0)
+
+    ffi.gc(plan, C.SPI_freeplan)
+    local prepared_plan = {plan = plan, oids = oids, argc = argc}
+    setmetatable(prepared_plan, plan_mt)
+
+    return prepared_plan
+    
+
+end
+
+
+
 function spi.disconnect()
-    C.SPI_finish()
+    return C.SPI_finish()
 end
 
 
