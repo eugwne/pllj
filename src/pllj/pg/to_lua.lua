52b5880157b3c053ac9fef8b7b00947d80a383bf
Eugene Sergeev <eugeney.sergeev@gmail.com>
2018-10-11 02:15:45 +0300

    INT4ARRAYOID (#21)

--- src/pllj/pg/to_lua.lua
+++ src/pllj/pg/to_lua.lua
@@ -3,9 +3,52 @@ local C = ffi.C
 require('pllj.pg.init_c')
 local macro = require('pllj.pg.macro')
 local call_pg_variadic = require('pllj.pg.func').call_pg_variadic
+local table_new = require('table.new')
+local NULL = ffi.NULL
 
 local typeto = {}
 
+
+typeto[C.INT4ARRAYOID] = function (datum)
+    assert(datum ~= nil)
+    local arr = ffi.cast('ArrayType *', macro.DatumGetArrayTypeP(datum)) 
+    local ndim = macro.ARR_NDIM(arr)
+    if ndim == 0 then
+        return {}
+    end
+    assert(ndim == 1)
+    local dims = macro.ARR_DIMS(arr)
+    if dims[0] == 0 then
+        return {}
+    end
+    local lbound = macro.ARR_LBOUND(arr)[0]
+    local typid = C.INT4OID --ARR_ELEMTYPE(arr);
+    local typlen = ffi.new("int16[?]", 1)
+    local typbyval = ffi.new("bool[?]", 1)
+    local typalign = ffi.new("char[?]", 1)
+
+    C.get_typlenbyvalalign(typid, typlen, typbyval, typalign)
+    local elemsp = ffi.new("Datum*[?]", 1)
+    local nullsp = ffi.new("bool*[?]", 1)
+    C.deconstruct_array(arr, typid, typlen[0], typbyval[0], typalign[0], elemsp, nullsp, dims);
+    
+    if dims[0] == 0 then
+        return {}
+    end
+    dims = dims[0]
+    local DatumGetInt32 = typeto[C.INT4OID]
+    local t = table_new(dims, 0)
+    for i = 0, dims -1 do
+        if nullsp[0][i] == true then
+            t[lbound] = NULL
+          else
+            t[lbound] = DatumGetInt32(elemsp[0][i]);
+          end
+          lbound = lbound + 1
+    end
+    return t
+
+end
 
 typeto[C.TEXTOID] = function (datum)
     --local d = C.DirectFunctionCall1Coll(C.textout, C.InvalidOid, datum)
@@ -15,7 +58,7 @@ end
 
 
 typeto[C.INT4OID] = function (datum)
-    return tonumber(datum)
+    return tonumber(macro.GET_4_BYTES(datum))
 end
 
 typeto[C.INT2OID] = function (datum)
--- src/pllj/pg/to_pg.lua
+++ src/pllj/pg/to_pg.lua
@@ -6,6 +6,78 @@ local NULL = ffi.NULL
 
 local datumfor = {}
 
+local szArrayType = ffi.sizeof('ArrayType')
+local szDatum = ffi.sizeof('Datum')
+datumfor[C.INT4ARRAYOID] = function (v)
+    if v == nil or v == NULL then
+        return ffi.cast('Datum', 0)
+    end
+    local type_name = type(v)
+    if (type_name == 'table') then
+        local ndims = -1
+        local lbound, ubound
+        local dims
+        for k, _ in pairs(v) do
+            if type(k) == 'number' then
+                if lbound then
+                    lbound = math.min(lbound, k)
+                    ubound = math.max(ubound, k)
+                 else
+                    lbound, ubound = k, k
+                 end
+            end
+        end
+
+        local size = 0
+        if ubound and lbound then
+            size = ubound - lbound + 1
+        end
+        if size == 0 then
+            local arr = ffi.cast('ArrayType*', C.SPI_palloc(szArrayType));
+            macro.SET_VARSIZE(arr, szArrayType);
+            arr.ndim = 0;
+            arr.dataoffset = 0;
+            arr.elemtype = C.INT4OID;
+            return ffi.cast('Datum', arr)
+        end
+        local ndims = 1
+        local elmtype = C.INT4OID
+        local d = ffi.cast('Datum *', C.SPI_palloc(szDatum * size));
+        local nulls = ffi.new("bool[?]", size)
+        local dims = ffi.new("int[?]", ndims)
+        local lbs = ffi.new("int[?]", ndims)
+        dims[0] = size
+        lbs[0] = lbound
+
+        local Int32GetDatum = datumfor[C.INT4OID]
+        local zidx = 0
+        for i = lbound, ubound do
+            local value = v[i]
+            if value == nil then
+                nulls[zidx] = true
+            else
+                d[zidx] = Int32GetDatum(value);
+            end
+            zidx = zidx + 1
+        end
+        local typlen = ffi.new("int16[?]", 1)
+        local typbyval = ffi.new("bool[?]", 1)
+        local typalign = ffi.new("char[?]", 1)
+    
+        C.get_typlenbyvalalign(elmtype, typlen, typbyval, typalign)
+
+        local prev = C.CurrentMemoryContext
+        C.CurrentMemoryContext = C.CurTransactionContext
+        --TODO try catch
+        local arr = C.construct_md_array(d, nulls, ndims, dims, lbs, elmtype, typlen[0], typbyval[0], typalign[0])
+        C.CurrentMemoryContext = prev
+
+        return ffi.cast('Datum', arr)
+
+    end
+    return error('NYI')
+end
+
 datumfor[C.TEXTOID] = function (v , p_isnull)
     if v == nil or v == NULL then
         if (p_isnull ~=nil) then
@@ -22,7 +94,6 @@ datumfor[C.TEXTOID] = function (v , p_isnull)
     --return ffi.string(ffi.cast('Pointer', d))
     return ffi.cast('Datum', out_ptr)
 end
-
 
 datumfor[C.INT4OID] = function (v)
     return ffi.cast('Datum',--[[SET_4_BYTES]](tonumber(v)))
